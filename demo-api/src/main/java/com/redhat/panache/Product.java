50e63c49974572bd8e05e1c0fa94e4d294e45db1
Nikhil Malvankar <nmalvank@Nikhils-MacBook-Pro.local>
2021-04-13 23:57:24 -0400

    first commit

---
+++ demo-api/src/main/java/com/redhat/panache/Product.java
@@ -0,0 +1,22 @@
+package com.redhat.panache;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+@Cacheable
+public class Product extends PanacheEntity {
+
+    @Column(length = 40, unique = true)
+    public String name;
+
+    public Product() {
+    }
+
+    public Product(String name) {
+        this.name = name;
+    }
+}
---
+++ demo-api/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO product(id, name) VALUES (nextval('hibernate_sequence'), 'Checking');
+INSERT INTO product(id, name) VALUES (nextval('hibernate_sequence'), 'Savings');
+INSERT INTO product(id, name) VALUES (nextval('hibernate_sequence'), 'Investments');
